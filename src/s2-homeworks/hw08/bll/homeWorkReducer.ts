e37aa914b0543cfdabfb0cb74d9dcccfe67e4dd8
Marina Zapasnik <zapasnikzapasnik@gmail.com>
2025-10-06 00:26:50 +0300

    hw8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,35 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: "sort"; payload: "up" | "down" }
+  | { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
+export const homeWorkReducer = (
+  state: UserType[],
+  action: ActionType
+): UserType[] => {
+  // need to fix any
 
-            return state // need to fix
-        }
-        case 'check': {
 
-            return state // need to fix
-        }
-        default:
-            return state
+
+  switch (action.type) {
+    case "sort": {
+      // by name
+      if (action.payload === "up") {
+       state.sort((a, b) => a.name.localeCompare(b.name));
+      }
+      if (action.payload === "down") {
+        state.sort((a, b) => b.name.localeCompare(a.name));
+      }
+      return state
+       // need to fix
     }
-}
+    case "check": {
+      return state.filter((el) => el.age >= 18);
+
+       // need to fix
+    }
+    default:
+      return state;
+  }
+};
